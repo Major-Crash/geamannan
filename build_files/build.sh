e1dc578e460ef95097ac5be9015ae25a83cde6cd
major-crash <majorcrash@thundermail.com>
2026-08-22 16:43:08 -0500

    Set specific firmware version

--- build_files/build.sh
+++ build_files/build.sh
@@ -30,7 +30,7 @@ dnf5 -y copr enable yalter/niri fedora-44-x86_64
 dnf5 -y install "${CUSTOM_PACKAGES[@]}"
 dnf5 -y copr disable avengemedia/dms
 dnf5 -y copr disable yalter/niri
-dnf5 -y downgrade realtek-firmware
+dnf5 -y downgrade realtek-firmware-0:20260622-1.fc44.noarch
 
 # Use a COPR Example:
 #
